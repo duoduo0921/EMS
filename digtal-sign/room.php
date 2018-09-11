cebcd5745feb6140e3a301b1aa368c4d939c09b6
handuo <handuo@Contractors-MacBook-Pro.local>
2018-09-11 15:28:59 -0400

    single room info

--- digtal-sign/room.php
+++ digtal-sign/room.php
@@ -1,180 +1,116 @@
 <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <title>Room Info</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </head>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Room Info</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</head>
 
-  <body>
+<body>
 
-  <?php
-  // EMS Variables defined as constants
-  try {
-      require 'EMS_VARIABLES.php';
-  } catch (Exception $e) {
-      exit('Require failed! Error: '.$e);
-  }
+<h1 class="text-center">Room: Gund 109</h1>
 
-  // gets all rooms info
-  $roomsparams =  array('UserName' => SERT_USERNAME, 'Password' => SERT_PASSWORD, 'BuildingID' => -1);
+<?php
+// EMS Variables defined as constants
 
-  // define new soap client object
-  $client = new SoapClient(SERT_SERVICE_URL);
+try {
+    require 'EMS_VARIABLES.php';
+} catch (Exception $e) {
+    exit('Require failed! Error: '.$e);
+}
 
-  $rooms = simplexml_load_string($client->GetAllRooms($roomsparams)->GetAllRoomsResult);
+// define new soap client object
+$client = new SoapClient(SERT_SERVICE_URL);
 
-  $roomInfo=array();
+?>
 
-  foreach ($rooms as $value) {
-      $id = (string)$value->ID;
-      $des = (string)$value->Description;
-      array_push($roomInfo, [$id, $des]);
-  }
-  sort($roomInfo);
-  array_shift($roomInfo);
+<?php
 
-  //print_r($roomInfo[60]);
-  ?>
+$bookingparams =  array('UserName' => SERT_USERNAME, 'Password' => SERT_PASSWORD,
+    'StartDate' => date( 'Y-m-d\T00:00:00' ), 'EndDate' => date("Y-m-d\TH:i:s",time()+(2 * 24 * 60 * 60)),
+    'RoomID' => 1, 'ViewComboRoomComponents' => TRUE);
+$bookinginfo = simplexml_load_string($client->GetAllRoomBookings($bookingparams)->GetAllRoomBookingsResult);
 
-  <h2 class="text-center">
-      <form action="#" method="post">
-          <select name="Room">
-              <option value=""></option>
-              <?php
-              foreach($roomInfo as $key => $value):
-                  echo '<option value="'.$key.'">'.$value[1].'</option>';
-              endforeach;
-              ?>
-          </select>
-          <button type="submit" name="submit" class="btn btn-dark">Get Bookings</button>
-      </form>
-      <?php
-      if(isset($_POST['submit'])){
-          $selected_val = $_POST['Room'];
-          echo "Room :" .$roomInfo[$selected_val][1];
-      }
-      ?>
+$listallbookings=array();
 
-  </h2><br/>
+foreach ($bookinginfo as $value) {
+    $groupname = (string)$value->GroupName;
+    $eventname = (string)$value->EventName;
+    $eventstart = (string)$value->TimeEventStart;
+    $eventend = (string)$value->TimeEventEnd;
+    $contact = (string)$value->ContactEmailAddress;
 
-  <?php
-  $rid = 0;
-  if(isset($_POST['submit'])){
-      $selected_val = $_POST['Room'];
-      $rid = $roomInfo[$selected_val][0];
-  }
+    if($value->StatusID != 1 && $value->StatusID != 7) {continue;}
+    if(!$eventstart) {continue;}
 
-  $bookingparams =  array('UserName' => SERT_USERNAME, 'Password' => SERT_PASSWORD,
-      'StartDate' => date( 'Y-m-d\T00:00:00' ), 'EndDate' => date("Y-m-d\TH:i:s",time()+(2 * 24 * 60 * 60)),
-      'RoomID' => $rid, 'ViewComboRoomComponents' => TRUE);
-  $bookinginfo = simplexml_load_string($client->GetAllRoomBookings($bookingparams)->GetAllRoomBookingsResult);
+    array_push($listallbookings,array($groupname, $eventname,
+        $eventstart, $eventend, $contact));
+}
 
-  $listallbookings=array();
+$uniqueBookings = array_unique($listallbookings, SORT_REGULAR);
+if (!$uniqueBookings) {
+    echo "<div class=\"alert alert-success col-12 col-sm-6 col-md-8 offset-md-2\" role=\"alert\">
+      <h4 class=\"alert-heading text-center\"> Available in two days! </h4>
+  </div>";
+    return;
+}
 
-  foreach ($bookinginfo as $value) {
-      $groupname = (string)$value->GroupName;
-      $eventname = (string)$value->EventName;
-      $eventstart = (string)$value->TimeEventStart;
-      $eventend = (string)$value->TimeEventEnd;
-      $contact = (string)$value->ContactEmailAddress;
+function check_in_range($start_date, $end_date, $date_from_user)
+{
+    // Convert to timestamp
+    $start_ts = strtotime($start_date);
+    $end_ts = strtotime($end_date);
+    $user_ts = strtotime($date_from_user);
 
-      if($value->StatusID != 1 && $value->StatusID != 7) {continue;}
-      if(!$eventstart) {continue;}
+    // Check that user date is between start & end
+    return (($user_ts >= $start_ts) && ($user_ts <= $end_ts));
+}
 
-      array_push($listallbookings,array($groupname, $eventname,
-          $eventstart, $eventend, $contact));
-  }
+$flag = false;
+$count = 0;
+foreach ($uniqueBookings as $value) {
+    $eventname = $value[1];
+    $eventstart = $value[2];
+    $eventend = $value[3];
 
-  $uniqueBookings = array_unique($listallbookings, SORT_REGULAR);
-  //sort($uniqueBookings);
-
-  function check_in_range($start_date, $end_date, $date_from_user)
-  {
-      // Convert to timestamp
-      $start_ts = strtotime($start_date);
-      $end_ts = strtotime($end_date);
-      $user_ts = strtotime($date_from_user);
-
-      // Check that user date is between start & end
-      return (($user_ts >= $start_ts) && ($user_ts <= $end_ts));
-  }
-
-  $flag = false;
-  $count = 0;
-  foreach ($uniqueBookings as $value) {
-      $eventname = $value[1];
-      $eventstart = $value[2];
-      $eventend = $value[3];
-
-      if (check_in_range($eventstart, $eventend, date( 'Y-m-d\TH:i:s'))) {
-          echo "<div class=\"alert alert-danger\" role=\"alert\">
-            <h4 class=\"alert-heading text-center\">Now Booked! </h4>
-            <p class='text-center'>( $eventname Until $eventend )</p>
+    if (check_in_range($eventstart, $eventend, date( 'Y-m-d\TH:i:s'))) {
+        echo "<div class=\"alert alert-danger col-12 col-sm-6 col-md-8 offset-md-2\" role=\"alert\">
+            <h4 class=\"alert-heading text-center\">Now Booked! </h4>.
+            <p class='text-center'> Current Event: $eventname </p>
+            <p class='text-center'> Until $eventend </p>
       </div>";
-          $count++;
-          $flag = true;
-          break;
-      }
-      $count++;
-  }
+        $count++;
+        $flag = true;
+        break;
+    }
+    $count++;
+}
 
-  while (!$uniqueBookings[$count]) {
-      $count++;
-  }
-  echo "<div class=\"alert alert-warning text-center\" role=\"alert\"> Next Event: ".$uniqueBookings[$count][1].
-      " from ". $uniqueBookings[$count][2]."</div>";
+while (!$uniqueBookings[$count]) {
+    $count++;
+}
+echo "<div class=\"alert alert-warning text-center col-12 col-sm-6 col-md-8 offset-md-2\" role=\"alert\"> 
+    Next Event: ".$uniqueBookings[$count][1]."</br>".
+    " From ". $uniqueBookings[$count][2]."</div>";
 
-  if (!$flag) {
-      foreach ($uniqueBookings as $value) {
-          $eventstart = $value[2];
-          $start_ts = strtotime($eventstart);
-          if ($start_ts >= strtotime(date( 'Y-m-d\TH:i:s'))) {
-              echo "<div class=\"alert alert-success\" role=\"alert\">
+if (!$flag) {
+    foreach ($uniqueBookings as $value) {
+        $eventstart = $value[2];
+        $start_ts = strtotime($eventstart);
+        if ($start_ts >= strtotime(date( 'Y-m-d\TH:i:s'))) {
+            echo "<div class=\"alert alert-success\" role=\"alert\">
             <h4 class=\"alert-heading text-center\"> Now Available! </h4>
             <p class='text-center'>( Until $eventstart ) </p>
             </div>";
-              break;
-          }
-      }
-  }
-  ?>
+            break;
+        }
+    }
+}
+?>
 
-  <?php
-  foreach ($uniqueBookings as $outputpiece) {
-      $output .= "<tr class=\"table-secondary\">"."<td>".$outputpiece[0]."</td>".
-          "<td>".$outputpiece[1]."</td>".
-          "<td>".$outputpiece[2]."</td>".
-          "<td>".$outputpiece[3]."</td>".
-          "<td>".$outputpiece[4]."</td>"."<tr>";
-
-  }
-  if ($output) {
-      echo "<h3 class='text-center'>Next two days: </h3>
-  <table class=\"table table-bordered\">
-      <thead class=\"thead-dark\">
-      <tr>
-          <th>Group Name</th>
-          <th>Event Name</th>
-          <th>Start Time</th>
-          <th>End Time</th>
-          <th>Contact</th>
-      </tr>
-      </thead>
-      <tbody>
-      $output
-      </tbody>
-  </table>";
-  } else {
-      echo "<div class=\"alert alert-success\" role=\"alert\">
-      <h4 class=\"alert-heading text-center\"> Available in two days! </h4>
-  </div>";
-  }
-  ?>
-
-  </body>
-  </html>
+</body>
+</html>
